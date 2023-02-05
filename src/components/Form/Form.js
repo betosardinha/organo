2769ab5d60dd1388160316f5b575812a8fbff535
Beto Sardinha <sardinhabeto55@gmail.com>
2023-02-05 14:39:27 -0300

    feat: add form validations

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,14 +13,18 @@ const Form = () => {
     'Management'
   ];
 
+  const onSave = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <section className='form'>
-      <form>
+      <form onSubmit={onSave}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <InputText label="Nome" placeholder="Digite seu nome" />
-        <InputText label="Cargo" placeholder="Digite seu cargo" />
-        <InputText label="Imagem" placeholder="Digite o endereço da imagem" />
-        <Select label="Time" items={teams}/>
+        <InputText required={true} label="Nome" placeholder="Digite seu nome" />
+        <InputText required={true} label="Cargo" placeholder="Digite seu cargo" />
+        <InputText required={true} label="Imagem" placeholder="Digite o endereço da imagem" />
+        <Select  required={true} label="Time" items={teams}/>
         <Button>
           Criar Card
         </Button>
--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -4,7 +4,7 @@ const InputText = (props) => {
   return (
     <div className="input-text">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder} />
+      <input placeholder={props.placeholder} required={props.required} />
     </div>
   );
 }
