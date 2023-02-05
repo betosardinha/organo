d27c039154d8ad592b574e4765c903099794693d
Beto Sardinha <sardinhabeto55@gmail.com>
2023-02-05 16:54:20 -0300

    feat: add team component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
   const [collaborators, setCollaborators] = useState([]);
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Banner />
       <Form onRegisteredCollaborator={collaborator => onNewCollaborator(collaborator)} />
+      <Team name="Front-End" />
     </div>
   );
 }
