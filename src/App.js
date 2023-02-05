5dc33ed12d3d725d9b91f435f4681c2d271dcf0d
Beto Sardinha <sardinhabeto55@gmail.com>
2023-02-05 18:55:38 -0300

    feat: add footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 import Form from './components/Form';
 import Team from './components/Team';
 
@@ -52,6 +53,7 @@ function App() {
       <Banner />
       <Form teams={teams.map(team => team.name)} onRegisteredCollaborator={collaborator => onNewCollaborator(collaborator)} />
       {teams.map(team => <Team key={team.name} name={team.name} primaryColor={team.primaryColor} secondaryColor={team.secondaryColor} collaborators={collaborators.filter(collaborator => collaborator.team === team.name)} />)}
+      <Footer />
     </div>
   );
 }
