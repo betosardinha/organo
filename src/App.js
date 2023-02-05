5da9d71df0e29f4bdfa4bf12638a4590ad51c069
Beto Sardinha <sardinhabeto55@gmail.com>
2023-02-05 17:50:16 -0300

    refactor: change team component for more teams

--- src/App.js
+++ src/App.js
@@ -4,6 +4,43 @@ import Form from './components/Form';
 import Team from './components/Team';
 
 function App() {
+  const teams = [
+    {
+      name: 'Programming',
+      primaryColor: '#57C278',
+      secondaryColor: '#D9F7E9',
+    },
+    {
+      name: 'Front-End',
+      primaryColor: '#82CFFA',
+      secondaryColor: '#E8F8FF',
+    },
+    {
+      name: 'Data Science',
+      primaryColor: '#A6D157',
+      secondaryColor: '#F0F8E2',
+    },
+    {
+      name: 'Devops',
+      primaryColor: '#E06B69',
+      secondaryColor: '#FDE7E8',
+    },
+    {
+      name: 'Design/UX',
+      primaryColor: '#DB6EBF',
+      secondaryColor: '#FAE9F5',
+    },
+    {
+      name: 'Mobile',
+      primaryColor: '#FFBA05',
+      secondaryColor: '#FFF5D9',
+    },
+    {
+      name: 'Management',
+      primaryColor: '#FF8A29',
+      secondaryColor: '#FFEEDF',
+    }
+  ];
   const [collaborators, setCollaborators] = useState([]);
 
   const onNewCollaborator = (collaborator) => {
@@ -14,7 +51,7 @@ function App() {
     <div className="App">
       <Banner />
       <Form onRegisteredCollaborator={collaborator => onNewCollaborator(collaborator)} />
-      <Team name="Front-End" />
+      {teams.map(team => <Team key={team.name} name={team.name} primaryColor={team.primaryColor} secondaryColor={team.secondaryColor} />)}
     </div>
   );
 }
